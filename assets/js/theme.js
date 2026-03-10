54a3b45f7f038b503766b26a2842be850167e445
Spiros Karagiannis <s.karagiannis@egov.demokritos.gr>
2026-03-10 16:17:44 +0200

    feedback form

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,13 +1,12 @@
 $t = jQuery.noConflict();
 
 $t(document).ready(function(){
-    $t(window).scroll(function () {
-        console.log($t(this).scrollTop())
+    $t('.main-content--body').scroll(function () {
         if ($t(this).scrollTop() < 200) {
-            $t(".main-content--header").removeClass("shadow", 1000);
+            $t(".main-content--header").removeClass("add-shadow", 1000);
         }
         else
-            $t(".main-content--header").addClass("shadow", 1000);
+            $t(".main-content--header").addClass("add-shadow", 1000);
     });
 });
 
