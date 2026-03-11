c74a80601e3fe8dcd3474376f35e29c9e6d4a7f6
Spiros Karagiannis <s.karagiannis@egov.demokritos.gr>
2026-03-11 15:57:02 +0200

    responsive & feedback email

--- handleFeedback.php
+++ handleFeedback.php
@@ -1,3 +1,36 @@
 <?php
 
-echo $_POST["improvements"];
+$to      = 'spherewebgr@gmail.com';
+$subject = 'New Support App Rating';
+$message = '<h2>New rating</h2>';
+foreach($_POST['rating'] as $key => $value ){
+
+    switch ($key) {
+        case 'question1':
+            $question = 'Πόσο ικανοποιημένος/η είστε από την εξυπηρέτηση που λάβατε;';
+            break;
+        case 'question2':
+            $question =  "Λύθηκε το πρόβλημά σας;";
+            break;
+        case 'question3':
+            $question = "Πόσο ικανοποιημένος/η είστε από τον χρόνο ανταπόκρισης;";
+            break;
+        case 'question4':
+            $question = "Ήταν η επικοινωνία σαφής και κατανοητή;";
+            break;
+    }
+    $message .= '<p>'.$question.'<strong> '.$value.'</strong></p>';
+}
+$message .= $_POST['improvements'];
+$headers = 'From: support@egov.demokritos.gr'       . "\r\n" .
+    "Content-type:text/html;charset=UTF-8" . "\r\n".
+    'X-Mailer: PHP/' . phpversion();
+
+
+
+try {
+    mail($to, $subject, $message, $headers);
+    header('Location: thank-you.html');
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
